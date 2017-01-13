105b5338620dbdc41d1069a470d5ad759b299d77
tanhimahmed <tanhim34@gmail.com>
2017-01-12 22:54:54 -0500

    Add files via upload

---
+++ CoinRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 rotation = new Vector3 (15, 30, 45);
+		transform.Rotate (rotation * Time.deltaTime);
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody player1;
+
+	// Use this for initialization
+	void Start () {
+		player1 = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 move = new newVector3 (moveHorizontal, 0.0f, moveVertical);
+
+		player1.AddForce (move * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Coin")
+			other.gameObject.SetActive(false);
+	}
+}
